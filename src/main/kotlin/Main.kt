fdb4b2a8fa01546c3dc7112084d0e469999860fb
hyhdb <kkminwoo@gmail.com>
2026-04-29 09:46:14 +0900

    4

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+package me.jko
+
+import Person
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    println("hello")
+
+    val person = Person(20, "John")
+    person.introduce()
+}
