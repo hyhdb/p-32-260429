3798ce8c51aa4bfa4f57feb39ee74fc7ca008c03
hyhdb <kkminwoo@gmail.com>
2026-04-29 12:24:08 +0900

    17

---
+++ src/main/kotlin/ch17/Main.kt
@@ -0,0 +1,10 @@
+package ch17
+
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
+
+
+fun main() {
+    Files.copy(File("a.txt").toPath(), File("a_copy.txt").toPath(), StandardCopyOption.REPLACE_EXISTING)
+}
