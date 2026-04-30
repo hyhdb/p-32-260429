082ad5857741cb4f8066f74473bb091877b5ea50
hyhdb <kkminwoo@gmail.com>
2026-04-30 09:25:45 +0900

    34-2

--- src/main/kotlin/ch34/Main.kt
+++ src/main/kotlin/ch34/Main.kt
@@ -1,14 +1,20 @@
 package ch34
 
-open class Animal { // 기본적으로 final이므로 open 필요
+open class Animal { // 이제 상속 가능
     open fun makeSound() {
         println("Some generic animal sound")
     }
 }
 
-class Dog : Animal() {
-    override fun makeSound() {
+open class Dog : Animal() {
+    override fun makeSound() { // 메서드 오버라이딩 가능
         println("Bark! Bark!")
+    }
+}
+
+class YellowDog : Dog() {
+    override fun makeSound() { // override 메서드는 기본적으로 open
+        println("Woof! Woof!")
     }
 }
 
