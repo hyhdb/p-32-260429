6ccf73b7dcbbc0d13f64a1dd50a4cca974bf1dd8
hyhdb <kkminwoo@gmail.com>
2026-04-30 10:30:47 +0900

    40

---
+++ src/main/kotlin/ch40/Main.kt
@@ -0,0 +1,10 @@
+package ch40
+
+class Person(val name: String, val age: Int) {
+    constructor(name: String) : this(name, 0) // 주 생성자 호출
+}
+
+fun main() {
+    val person1 = Person("Alice", 25)
+    val person2 = Person("Bob") // age는 기본값 0
+}
